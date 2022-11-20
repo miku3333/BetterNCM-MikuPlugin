948efe2e1869ca58627e64c2a760344ac7ef1fb4
miku <390505764@qq.com>
2022-11-21 03:27:20 +0800

    feat: init

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import { RunTimeLayoutConfig } from '@umijs/max';
 import './style/index.less';
 
-export const layout = () => {
+export const layout: RunTimeLayoutConfig = initialState => {
     return {
         logo: 'https://cdn.staticaly.com/gh/miku3333/image-hosting@master/20221120/81689532_p0_master1200.6x9fnqz4hk40.webp',
         menu: {
             locale: false
-        }
+        },
+        defaultCollapsed: true,
+        breakpoint: false
     };
 };
 
@@ -17,15 +20,3 @@ export const qiankun = {
         console.log('miku-plugin mount');
     }
 };
-
-// export function render(oldRender: any) {
-//     const reactRoot = document.querySelector('#miku-plugin-root') || document.createElement('div');
-//     reactRoot.id = 'miku-plugin-root';
-//     document.body.appendChild(reactRoot);
-//     // @ts-ignore
-//     // console.log(root);
-//     console.log('umi render start');
-//     console.log(oldRender);
-//     console.log(oldRender());
-//     console.log('umi render end');
-// }
